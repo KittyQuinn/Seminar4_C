99462328c6506ea0ac1f77f98565da282ab3e631
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-10-01 00:00:32 +0300

    Task 30

---
+++ Task30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выводит массив из 8 элементов заполненный 0 и 1
+// в случайном порядке
+
+
+int [] arr = new int [8];
+
+for (int i=0; i<arr.Length; i++)
+{
+    arr[i] = new Random().Next(0,2);
+    Console.WriteLine(arr[i]);
+}
+
