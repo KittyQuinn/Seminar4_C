da7909fac352106e652a7884e82074270ce3c49d
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-30 22:44:50 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает число и выдает кол-во цифр в числе
+
+Console.Clear();
+Console.WriteLine("Type a number ");
+int num = int.Parse(Console.ReadLine());
+
+string A = Convert.ToString(num);
+
+Console.WriteLine(A.Length);
