0438be15d44a3ad966c54062b1491fd247e3646f
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-30 21:53:48 +0300

    Task 24

---
+++ Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает число А и выдает сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+
+Console.Clear();
+Console.WriteLine("Type a number ");
+int A = int.Parse(Console.ReadLine());
+
+int i;
+int result = 0;
+for (i = 1; i <= A; i++)
+{
+    result = result+i;
+}
+
+
+Console.WriteLine(result);
